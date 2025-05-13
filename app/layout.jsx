babfa40a17de5742d393d39e5c42f2d0d2507c3f
Funmilola Ariyo <lawlahruth@gmail.com>
2025-05-12 22:24:32 -0300

    added backlink

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,6 +20,19 @@ const RootLayout = ({children}) => {
                     <Nav />
                     {children}
                 </main>
+                <div className=" flex items-center justify-center">
+                    <div className="text-gray-400 text-center pb-10">
+                        Web design by{" "}
+                        <a
+                        href="https://madewithlux.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-500 hover:underline"
+                        >
+                        MadeWithLux
+                        </a>
+                    </div>
+                </div>
             </Provider>
         </body>
        
